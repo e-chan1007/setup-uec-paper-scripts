59a0e04a8970c5420ae09ad4f79729c1435bfdcd
いーちゃん(e_chan1007) <echan041007@gmail.com>
2024-04-10 23:32:28 +0900

    chore: add collection-binextra

--- unix.sh
+++ unix.sh
@@ -74,6 +74,7 @@ installTeXLive () {
   collection-langjapanese 1
   collection-latexextra 1
   collection-mathscience 1
+  collection-binextra 1
 
   tlpdbopt_install_docfiles 0
   tlpdbopt_install_srcfiles 0
--- windows.ps1
+++ windows.ps1
@@ -48,6 +48,7 @@ selected_scheme scheme-custom
 collection-langjapanese 1
 collection-latexextra 1
 collection-mathscience 1
+collection-binextra 1
 
 tlpdbopt_install_docfiles 0
 tlpdbopt_install_srcfiles 0
